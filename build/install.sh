fe4951524dda99fb4aba8a8a1802bc77d4e23e4d
J42 <julian@j42.me>
2015-05-14 02:43:54 -0400

    Fix reference to fork

--- build/install.sh
+++ build/install.sh
@@ -1,4 +1,4 @@
-git clone git://github.com/Vaizard/MurmurHash3PHP.git ~/tmp/murmurhash
+git clone git://github.com/j42/MurmurHash3PHP.git ~/tmp/murmurhash
 cd ~/tmp/murmurhash && phpize && ./configure --enable-murmurhash3 && make install
 [ -s /etc/php5/fpm/conf.d/20-murmurhash3.ini ] && echo "murmurhash3.ini already exists" || echo extension=murmurhash3.so | tee -a /etc/php5/fpm/conf.d/20-murmurhash3.ini
 [ -s /etc/php5/cli/conf.d/20-murmurhash3.ini ] && echo "murmurhash3.ini already exists" || echo extension=murmurhash3.so | tee -a /etc/php5/cli/conf.d/20-murmurhash3.ini
